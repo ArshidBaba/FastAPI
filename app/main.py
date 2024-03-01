99890a254424cc131851048c8d013c9e00fda7db
Arshid <arshidbaba93@gmail.com>
2024-03-01 20:27:52 +0530

    POST endopoint

--- app/main.py
+++ app/main.py
@@ -46,6 +46,25 @@ def search_recipes(
     return {"results": list(results)[:max_results]}
 
 
+@api_router.post("/recipe/", status_code=201, response_model=Recipe)
+def create_recipe(*, recipe_in: RecipeCreate) -> dict:
+    """
+    Create a new recipe (in memory only)
+    """
+
+    new_entry_id = len(RECIPES) + 1
+    recipe_entry = Recipe(
+        id=new_entry_id,
+        label=recipe_in.label,
+        source=recipe_in.source,
+        url=recipe_in.url,
+    )
+
+    RECIPES.append(recipe_entry.dict())
+
+    return recipe_entry
+
+
 app.include_router(api_router)
 
 if __name__ == "__main__":

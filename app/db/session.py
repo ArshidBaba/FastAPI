e8811761a11d442a54caebbba0c214ff0025c7cc
Arshid <arshidbaba93@gmail.com>
2024-03-03 23:29:20 +0530

    Added Database functionality

--- app/db/session.py
+++ app/db/session.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///example.db"
 
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URI,
+    # required for sqlite
     connect_args={"check_same_thread": False},
 )
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
